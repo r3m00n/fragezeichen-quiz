44c75a29032874e2b489e10902b5fbbd4fbad4ce
Merlin Förster <moin@merlin.hamburg>
2023-03-30 12:53:04 +0200

    added audio question helper

--- frontend/helpers/audioQuestionHelper.ts
+++ frontend/helpers/audioQuestionHelper.ts
@@ -1,3 +1,20 @@
-import {Episode, Quiz} from "../types/types"
+import {AnswerType, Episode, Question, QuestionType, Quiz} from "../types/types"
 
-export const getAudioQuestions = (episodes: Episode[]): Quiz => {}
+export const getAudioQuestions = (episodes: Episode[]): Quiz => {
+    const episodesWithAudio = episodes.filter(e => e.mp3_urls.length)
+    return episodesWithAudio.flatMap(episode =>
+        episode.mp3_urls.split(";").map((url, index) => ({
+            id: `audio_${episode.id}.${index}`,
+            structure: {
+                id: 2,
+                questionType: QuestionType.audio,
+                answerType: AnswerType.string
+            },
+            question: "Wie heißt die zu hörende Folge?",
+            answer: episode.title,
+            metaData: {
+                audio_url: url
+            }
+        }))
+    )
+}
